587b26392c04f8b62bc0ea95e04fbf306a7307a7
dan murphy <kingstash@gmail.com>
2019-09-09 14:09:18 -0700

    Added study drill code to lpthw_python3_ex7.py

---
+++ lpthw_python3_ex7.py
@@ -0,0 +1,41 @@
+# this is a print function
+print("Mary had a little lamb.")
+
+# this is a print function using the format function
+print("Its fleece was white as snow as {}.".format('snow'))
+
+# this is a print function
+print("And everywhere that Mary went.")
+
+# this a print function that makes ten dots in a row
+print("."* 10) # repeats the . ten times
+
+# variable that holds the letter C
+end1 = "C"
+# variable that holds the letter h
+end2 = "h"
+# variable that holds the letter e
+end3 = "e"
+# variable that holds the letter e
+end4 = "e"
+# variable that holds the letter s
+end5 = "s"
+# variable that holds the letter e
+end6 = "e"
+# variable that holds the letter B
+end7 = "B"
+# variable that holds the letter u
+end8 = "u"
+# variable that holds the letter r
+end9 = "r"
+# variable that holds the letter g
+end10 = "g"
+# variable that holds the letter e
+end11 = "e"
+# variable that holds the letter r
+end12 = "r"
+
+#print function that prints cheese with a space at the end
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+#print function that prints burger 
+print(end7 + end8 + end9 + end10 + end11 + end12)
