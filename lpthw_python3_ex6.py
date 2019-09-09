4381abc4c9be5afa775e4e9059cb91c0032233ce
dan murphy <kingstash@gmail.com>
2019-09-09 08:59:22 -0700

    Added study drill code to lpthw_python3_ex6.py

---
+++ lpthw_python3_ex6.py
@@ -0,0 +1,41 @@
+# this is a variable 
+types_of_people = 10
+# this variable holding a f-string format
+x = f"there are {types_of_people} types of people."
+
+# this is a variable
+binary = "binary"
+# this is a variable
+do_not = "don't"
+
+# this variable holding a f-string format
+y = f"Those who know {binary} and those who {do_not}"
+
+# this is a print function , printing out variable x
+print(x)
+
+# this is a print function , printing out variable y
+print(y)
+
+# this is a print function, with an f-string format. printing out x variable
+print(f"I said :{x}")
+
+# this is a print function, with an f-string format. printing out y variable
+print(f"I also said: {y}")
+
+# this is a variable holding a boolean function of False
+hilarous = False
+# this is a varaiable holding a string with curly braces to be used with the format() function
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# this print() function is printing out joke_evaluation will using the format() function
+print (joke_evaluation.format(hilarous))
+
+# this is a variable holding a string
+w = "This is the left side of ..."
+
+# this is a variable holding a string
+e = "a string with a right side."
+
+# this print() function is printing out two strings while concatenateing both strings together
+print(w + e)
