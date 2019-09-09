6bba5be41973250bc74510cd0de4b210b0c19df1
dan murphy <kingstash@gmail.com>
2019-09-09 05:53:18 -0700

    Corrected the code and commits to match centimeters and kilograms

--- lpthw_python3_ex5.py
+++ lpthw_python3_ex5.py
@@ -1,18 +1,18 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
-my_cent = 74 * 2.54 # inches
-my_weight = 180 # lbs
+my_cent = 74 * 2.54 # converting inches to centimeters
+my_kg = 180 / 0.45359237# converting pounds to kg
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 print(f"Let's talk about {my_name}")
 print(f"He's {my_cent} centimeters tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"He's {my_kg} pounds heavy.")
 print(f"Actually that's not too heavy.")
 print(f"He's got {my_eyes}eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"if I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = my_age + my_kg + my_kg
+print(f"if I add {my_age}, {my_kg}, and {my_kg} I get {total}.")
