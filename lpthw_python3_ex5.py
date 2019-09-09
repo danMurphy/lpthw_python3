6c54eb578d9074d0fa67dfb35822d031404fcbb5
dan murphy <kingstash@gmail.com>
2019-09-09 04:12:07 -0700

    Added study drill code to lpthw_python3_ex5.py

--- lpthw_python3_ex5.py
+++ lpthw_python3_ex5.py
@@ -1,13 +1,13 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
-my_height = 74 # inches
+my_cent = 74 * 2.54 # inches
 my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 print(f"Let's talk about {my_name}")
-print(f"He's {my_height} inches tall.")
+print(f"He's {my_cent} centimeters tall.")
 print(f"He's {my_weight} pounds heavy.")
 print(f"Actually that's not too heavy.")
 print(f"He's got {my_eyes}eyes and {my_hair} hair.")
