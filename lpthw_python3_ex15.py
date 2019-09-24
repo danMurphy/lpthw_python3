b98109a6b6333fa22ffffb5261582af8e8d30b0d
dan murphy <kingstash@gmail.com>
2019-09-24 13:15:38 -0700

    Added practice code to lpthw_python3_ex15.py plus the sample.txt file

---
+++ lpthw_python3_ex15.py
@@ -0,0 +1,24 @@
+# import argv from sys module
+from sys import argv
+
+# unpacking script variable and filename variable 
+script, filename = argv
+
+# variable named txt using the open function to open passed argument of filename
+txt = open(filename)
+
+#printing out the filename
+print(f"Here is file : {filename}")
+#printing out the file contents here
+print(txt.read())
+
+# printing out type the filename again 
+print("Type the filename again : ")
+#getting the input of the filename again
+file_again = input('> ')
+
+#variable here using the open function to open file
+txt_again = open(file_again)
+
+#printing the contents again of the filei
+print(txt_again.read())
