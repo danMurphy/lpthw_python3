e8e220885c2029bdaef28446e8a16224817182c6
Dan <kingstash@gmail.com>
2020-10-17 21:01:58 -0400

    Added practice code lpthw_python_ex28.py

---
+++ lpthw_python3_ex28.py
@@ -0,0 +1,62 @@
+a = True and True
+print(a)
+
+b = False and True
+print(b)
+
+c = 1 == 1 and 2 == 1
+print(c)
+
+d = "test" == "test"
+print(d)
+
+e = 1 == 1 or 2 != 1
+print(e)
+
+f = True and 1 == 1
+print(f)
+
+g = False and 0 != 0
+print(g)
+
+h = True or 1 == 1
+print(h)
+
+i = "test" == "testing"
+print(i)
+
+j = 1 != 0 and 2 == 1
+print(j)
+
+k = "test" == 1
+print(k)
+
+l = not(True and False)
+print(l)
+
+m = not(1 == 1 and 0 != 1)
+print(m)
+
+n = (10 == 1 or 1000 == 1000)
+print(n)
+
+o = not(1 != 10 or 3 == 4)
+print(o)
+
+p = 1 == 1 or 3 == 4
+print(p)
+
+q = 3 == 4 or 1 == 1
+print(q)
+
+s = not("testing" == "testing" and "Zed" == "Cool Guy")
+print(s)
+
+t = 1 == 1 and (not ("testing" ==  1 or 1 == 0))
+print(t)
+
+u = "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+print(u)
+
+v = 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+print(v)
